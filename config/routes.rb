b53acaf37725f189382d4d06fcf169f9e4be2a0a
Garry <garry.tamhuang@gmail.com>
2017-05-20 00:44:15 +0700

    Assignment 8: Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  resources :advertisements
 
   resources :posts
 
   get 'about' => 'welcome#about'
 
-  root 'welcome#index'
+  root to: 'welcome#index'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
