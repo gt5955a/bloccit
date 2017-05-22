43a85018a9cf384ee3db13db7c0d97d03ce2a9f7
Garry <garry.tamhuang@gmail.com>
2017-05-22 17:39:04 +0700

    Checkpoint 10: Completed

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
-  resources :advertisements
+  resources :topics do
+    resources :posts, except: [:index]
+  end
 
-  resources :posts
+  resources :advertisements
 
   resources :questions
 
